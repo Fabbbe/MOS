cace5b6353c29ff1477a27685be8016294f3c87a
Fabbbe <fabbbe166@gmail.com>
2019-10-02 13:29:08 +0200

    Added disk_read function

--- boot/boot_sect.asm
+++ boot/boot_sect.asm
@@ -1,16 +1,30 @@
 ;
 ; Entry point for bios that currently does nothing
 ;
-
 [org 0x7c00]
 
 entry: ; Entrypoint of BIOS
-	mov bp, 0x8000
+
+	; Disk reading functions
+	mov [BOOT_DRIVE], dl ; BIOS stores boot dirve in DL, so we can store it 
+						 ; in memory
+
+	mov bp, 0x8000 		; stack pointer address
 	mov sp, bp
 
+	mov bx, 0x9000 
+	mov dh, 5			; Load 5 sectors to 0x0000:0x9000(ES:BX)
+	mov dl, [BOOT_DRIVE]; Load from boot drive
+	call disk_load
+
 	pusha 
-	mov dx, 0x0919 		; arbitrary number
+	mov dx, [0x9000] 	; load data we loaded from disk
 	call print_hex		; dx is hex to print
+	popa
+
+	pusha 
+	mov dx, [0x9000 + 512]; load data we loaded from disk
+	call print_hex		  ; dx is hex to print
 	popa
 
 	pusha 				; Push all registers to the stack
@@ -20,14 +34,16 @@ entry: ; Entrypoint of BIOS
 
 	jmp the_end 		; Jump past all code
 
-
+%include "./boot/disk_load.asm"
 %include "./boot/bios_print.asm"
-
 
 ; DATA SECTION
 
 BOOT_MSG:
-	db ' Booting MOS...', 0 ; Null terminated string
+	db 'Booting MOS...', 0 ; Null terminated string
+
+BOOT_DRIVE: db 0
+
 
 the_end:
 
@@ -38,3 +54,5 @@ the_end:
 times 510-($-$$) db 0
 
 dw 0xaa55
+times 256 dw 0xdead 
+times 256 dw 0xbeef
---
+++ boot/disk_load.asm
@@ -0,0 +1,27 @@
+disk_load:
+	push dx
+
+	mov ah, 0x02		; BOIS read sector function
+	mov al, dh			; Read DH amount of sectors
+	mov ch, 0x00		; Select cylinder 0
+	mov dh, 0x00		; Select head 1 of the disk since it starts at 0
+	mov cl, 0x02		; Select the 2nd (after the boot sector)
+
+	int 0x13			; BIOS interrupt for read disk function
+
+	jc disk_error 		; Jump to disk_error if BIOS failed to read disk
+
+	pop dx
+	cmp dh, al
+	jne disk_error
+	ret
+
+disk_error:
+	;pusha 				; Push all registers to the stack
+	mov bx, DISK_ERROR_MSG
+	call print
+	;popa
+	jmp the_end
+
+DISK_ERROR_MSG:
+	db 'COULD NOT READ DISK', 0
